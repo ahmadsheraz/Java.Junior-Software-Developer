806aad7f62fb12fd5873ee27d997322dc0f28bca
Sheraz ahmad <42497491+ahmadsheraz@users.noreply.github.com>
2018-08-18 22:44:52 +0200

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld{
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("This is my first Program to Push to GitHub Reprository");
-		system.out.println("This is my first code to Full from GitHub Reproditory");
+		system.out.println("This is my first code to Pull from GitHub Reproditory");
 	}
 
 }
